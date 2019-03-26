88bfad6e75d3e4cb7e416bd89091fa3d0b596fed
xishihao <xishihao@chehejia.com>
2019-03-26 09:06:25 +0800

    kafka 发送接收简单测试

--- spring-boot-kafka-study/src/main/java/org/xi/study/controller/HomeController.java
+++ spring-boot-kafka-study/src/main/java/org/xi/study/controller/HomeController.java
@@ -18,12 +18,12 @@ public class HomeController {
     public String callback(@PathVariable("event") String event, @RequestBody String data) throws Exception {
 
         JSONObject json = new JSONObject();
-        json.put("business", "huanxin");
+        json.put("business", "test");
         json.put("event", event);
         json.put("data", data);
 
         ListenableFuture<SendResult> listenableFuture = kafkaTemplate.send("callback", json.toJSONString());
-        listenableFuture.addCallback(result -> System.out.println("环信数据接收成功"), result -> System.out.println("环信数据接收失败"));
+        listenableFuture.addCallback(result -> System.out.println("数据接收成功"), result -> System.out.println("数据接收失败"));
         return "";
     }
 }
